aa25ba585c6189c21e0dcce4ae29ead55e1b0d74
yoonlang <cdt9473@gmail.com>
2022-09-22 21:44:25 +0900

    feat: Update head data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Script from 'next/script';
 import { RecoilRoot } from 'recoil';
 import Checker from 'components/Checker';
 import Nav from 'components/Nav';
+import { Head } from 'next/document';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +15,19 @@ function MyApp({ Component, pageProps }: AppProps) {
       />
       <GlobalStyle />
       <RecoilRoot>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="핀을 꽂아 의견을 말해요." />
+          <meta property="og:url" content="https://www.pinplaint.com/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="핀플레인" />
+          <meta property="og:description" content="핀을 꽂아 의견을 말해요." />
+          {/* <meta
+            property="og:image"
+            content="https://github.com/CSE-seat/CSE-Seat/blob/main/frontend/public/images/cse.png?raw=true"
+          /> */}
+          {/* <link rel="shortcut icon" type="image/x-icon" href="images/cse.png" /> */}
+        </Head>
         <Checker />
         <Nav />
         <Component {...pageProps} />
