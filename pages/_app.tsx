351d9b8e55cb88f24c4ab20876756e3a8bbfdfb2
yoonlang <cdt9473@gmail.com>
2022-09-25 02:59:53 +0900

    feat: Implement meta data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,17 +16,18 @@ function MyApp({ Component, pageProps }: AppProps) {
       <GlobalStyle />
       <RecoilRoot>
         <Head>
+          <title>핀플레인</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" content="핀을 꽂아 의견을 말해요." />
           <meta property="og:url" content="https://www.pinplaint.com/" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="핀플레인" />
           <meta property="og:description" content="핀을 꽂아 의견을 말해요." />
-          {/* <meta
+          <meta
             property="og:image"
-            content="https://github.com/CSE-seat/CSE-Seat/blob/main/frontend/public/images/cse.png?raw=true"
-          /> */}
-          {/* <link rel="shortcut icon" type="image/x-icon" href="images/cse.png" /> */}
+            content="https://avatars.githubusercontent.com/u/113536307?s=400&u=d97cf2f4647d904a1cfcd14cac0f92bc65b705e6&v=4"
+          />
+          <link rel="shortcut icon" type="image/x-icon" href="/pinplain.png" />
         </Head>
         <Checker />
         <Nav />
