05ee724cd129a43e7c5ea558dca382d6ea46d6c8
yoonlang <cdt9473@gmail.com>
2022-09-25 05:08:32 +0900

    feat: Implement buttons of map

--- src/components/MapServices.tsx
+++ src/components/MapServices.tsx
@@ -3,6 +3,10 @@ import { RefObject, Ref, Dispatch, SetStateAction } from 'react';
 import { closeAtom, geolocationAtom, realTimeAtom } from 'others/stateStore';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import styled from 'styled-components';
+import Pencil from '/public/pencil.svg';
+import Plus from '/public/plus.svg';
+import Minus from '/public/minus.svg';
+import RealTime from '/public/realTime.svg';
 import { colorByCategory, defaultPos } from '../constants/default';
 import { Category, Path } from 'others/IntegrateInterfaces';
 
@@ -69,15 +73,39 @@ const MapServices: React.FC<MapServicesProps> = ({ mapRef, path, setPosData, set
     });
   };
 
+  const handlePlus = () => {
+    const map = mapRef.current;
+    if (!map) return;
+    const nowLevel = map.getLevel();
+    map.setLevel(nowLevel - 1);
+  };
+
+  const handleMinus = () => {
+    const map = mapRef.current;
+    if (!map) return;
+    const nowLevel = map.getLevel();
+    map.setLevel(nowLevel + 1);
+  };
+
   return (
     <StyledMapServices choicedPin={choicedPin ?? 'LIFE'}>
-      <button
-        className={`realTimeBtn ${isRealTime ? 'realTime' : 'nonRealTime'}`}
-        onClick={handleRealTimeValue}
-      ></button>
+      <div className={'option'}>
+        <button onClick={handleRealTimeValue}>
+          <RealTime fill={isRealTime ? '#1A95FF' : '#4F4F4F'} />
+        </button>
+        <button onClick={handlePlus}>
+          <Plus />
+        </button>
+        <button onClick={handleMinus}>
+          <Minus />
+        </button>
+      </div>
       {path === 'map' && (
         <button className={'complain'} onClick={directToWritingPage}>
-          민원 넣기
+          <div>
+            <Pencil fill={'#F5564E'} />
+          </div>
+          <p>민원 작성하기</p>
         </button>
       )}
       {path === 'writing' && choicedPin && (
@@ -104,6 +132,31 @@ const StyledMapServices = styled.div<StyledMapServicesProps>`
   left: 0;
   width: 100%;
   height: 100vh;
+  & .option {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    width: 48px;
+    height: 144px;
+    background: #ffffff;
+    opacity: 0.9;
+    filter: drop-shadow(3px 3px 10px rgba(0, 0, 0, 0.25));
+    border-radius: 5px;
+    z-index: 2;
+    & > button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: none;
+      outline: none;
+      background: none;
+      width: 100%;
+      height: 100%;
+      cursor: pointer;
+    }
+  }
   & > button {
     position: absolute;
     z-index: 2;
@@ -125,10 +178,29 @@ const StyledMapServices = styled.div<StyledMapServicesProps>`
     background: red;
   }
   & .complain {
-    top: 20px;
-    right: 90px;
-    width: 50px;
-    height: 50px;
+    display: flex;
+    align-items: center;
+    width: 124px;
+    height: 48px;
+    top: 16px;
+    right: 80px;
+    background: #fff;
+    filter: drop-shadow(3px 3px 10px rgba(0, 0, 0, 0.25));
+    border-radius: 5px;
+    & > div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 48px;
+      height: 48px;
+    }
+    & > p {
+      position: relative;
+      left: -4px;
+      font-weight: 700;
+      font-size: 12px;
+      color: #4f4f4f;
+    }
   }
 
   & .selectPos {
